d8d77c1cb5b632039163939716033bd8ee8609ec
APikielny <apikielny@gmail.com>
2020-04-23 16:00:42 -0400

    model architecture

--- model/model_architecture.py
+++ model/model_architecture.py
@@ -12,26 +12,33 @@ from loss import L1
 EPOCHS = 1
 
 
-def train(self, model, optimizer):
+def train(model, optimizer):
     #1. batch data
+    for i in range(len(data)):
+        #2. zero optimizer
+        optimizer.zero_grad()
 
-    #2. zero optimizer
-    optimizer.zero_grad()
+        #3. forward pass
+        #hourglassNet forward pass requires: (self, x, target_light, skip_count)
+        #for skip count i think it depends on the epoch? ¯\_(ツ)_/¯
+        output = model.forward(data, more_data, skip_count)
 
-    #3. forward pass
-    #hourglassNet forward pass requires: (self, x, target_light, skip_count)
-    #for skip count i think it depends on the epoch? ¯\_(ツ)_/¯
-    output = model.forward(data, more_data, skip_count)
+        #4. loss + ~optimize~
+        #calculate num pixels for loss:
+        N = data[0].size ** 2
 
-    #4. loss
-    #calculate num pixels for loss:
-    N = data[0].size ** 2
-
-    loss = L1(N, data, data, data, data)
+        loss = L1(N, data, data, data, data)
+        loss.backward()
+        optimizer.step()
 
 
 model = HourglassNet()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) #not sure if we should be using Adam or another optimizer
 
- for i in range(EPOCHS):
-     train(model)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9) #not sure if we should be using Adam or another optimizer
+
+for i in range(EPOCHS):
+    train(model, optimizer)
+
+#Save model
+#https://stackoverflow.com/questions/42703500/best-way-to-save-a-trained-model-in-pytorch
+#Not sure if this^ is the best way, I think we should make sure to match the way we save if we want to run their test code
