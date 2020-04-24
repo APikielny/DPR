7c232afaa96b81b7533251c8acf05893b889d5b2
Ben Givertz <bgivertz@gmail.com>
2020-04-24 17:50:37 -0400

    testing single image

--- model/loss.py
+++ model/loss.py
@@ -5,8 +5,8 @@ def L1(N, I_t, I_tp, L_s, L_sp):
 
     # Fix norms
     img_norm = torch.norm(I_t - I_tp)
-    grad_norm = torch.norm(filters.gaussian(I_t) - filters.gaussian(I_tp))
-    image_loss = img_norm + grad_norm
+   #grad_norm = torch.norm(filters.gaussian(I_t) - filters.gaussian(I_tp))
+    image_loss = img_norm #+ grad_norm
 
     light_loss = (L_s - L_sp) ** 2
 
